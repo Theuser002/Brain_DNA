2a70defafb69bb330e3ddb4ba012714296f8a122
unknown <trihung18041999@gmail.com>
2022-07-20 02:29:54 +0700

    Update/Web autodectect fold

--- Web/pages/02_♦️_[Dev]-Alg-Model-Settings.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import web_config
-
-st.sidebar.markdown('# [Dev] Alg Model Settings')
-st.sidebar.selectbox('Choose model', web_config.algs)
--- Web/web_config.py
+++ Web/web_config.py
@@ -2,9 +2,9 @@ import sys
 import os
 
 outer_folds = ['1.0', '2.0', '3.0', '4.0', '5.0']
-temp_fold = '1.1'
+inner_fold_indexes = ['.1', '.2', '.3', '.4', '.5']
 algs = ['MLP', 'RF', 'LR', 'XGB']
 tissue_groups = [
     'Embryonal', 'Glioblastoma', 'Glio-neuronal', 'Sella', 'Ependymal', 'Other glioma', 'Nerve', 'Pineal', 'Mesenchymal', 'Melanocytic', 'Plexus', 'Glioma IDH', 'Haematopoietic', 'Unknown'
 ]
-cpg_info_path = '../Brain_DNA/Supplementary/humanmethylation450.csv'
+cpg_info_path = '../Supplementary/humanmethylation450.csv'
