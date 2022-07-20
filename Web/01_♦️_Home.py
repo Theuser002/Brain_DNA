9476956603f0dad75dc42d04b074b4f33335265e
unknown <trihung18041999@gmail.com>
2022-07-21 03:32:53 +0700

    Update/Web UI

--- Web/01_♦️_Home.py
+++ Web/01_♦️_Home.py
@@ -32,14 +32,14 @@ def get_impf_dataframe(df, outer_fold = '1.0', alg = 'RF', group = 'Embryonal'):
     return df, len(impf)
 
 @st.cache
-def get_impf(outer_fold = '1.0', alg = 'RF', group = 'Embryonal'):
-        with open(os.path.join(impf_cfg['IMPORTANT_FEATURES_DIR'], alg, group, f'{outer_fold}_combined.pkl'), 'rb') as file:
-            impf = pickle.load(file)
-        return impf
+def load_impf(outer_fold = '1.0', alg = 'RF', group = 'Embryonal'):
+    with open(os.path.join(impf_cfg['IMPORTANT_FEATURES_DIR'], alg, group, f'{outer_fold}_combined.pkl'), 'rb') as file:
+        impf = pickle.load(file)
+    return impf
 
 @st.cache
-def check_contain_features(df, impf):
-    return set(df.columns).issuperset(impf)
+# def check_contain_features(df, impf):
+    # return set(df.columns).issuperset(impf)
 
 @st.cache
 def get_ranked_impf(df, outer_fold = '1.0', alg = 'RF', group = 'Embryonal'):
@@ -55,117 +55,97 @@ st.sidebar.markdown('# Home')
 
 title = 'DNA methylation-based classification of central nervous system tumours'
 st.title(title, 'homepage')
-st.write('---')
+st.write('---  \n---')
 st.write('### File upload')
 uploaded_file = st.file_uploader('UPLOAD A CSV FILE')
 try:
     st.write(f'File ***{uploaded_file.name}*** received.')
     try:
-        st.write('---')
+        st.write('---  \n---')
         st.write('### CpG filtering')
         # df = pd.read_csv(uploaded_file, nrows=1)
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file, index_col=0)
         n_samples = df.shape[0]
-        
-        if df.shape[0] > 1:
-            sample_index = st.selectbox('Multiple samples detected, select the sample you want to get diagnosed:', range(n_samples))
-        else:
-            sample_index = 0
 
-        df = pd.DataFrame(df.iloc[sample_index,:]).T
-        st.write('**Sample:**')
-        st.write(df)
         # Get user's input
+        form_0_submitted = False
         with st.form(key = 'form_0'):
+            sample_index = st.selectbox('Choose ur sample: ', df.index)
             alg = st.selectbox('Select important features algorithm: ', algs)
             group = st.selectbox('Select tissue origin: ', tissue_groups)
-            # outer_fold = st.selectbox('Select fold (development only): ', outer_folds)
-            # inner_fold_index = st.selectbox("Select inner fold's index", inner_fold_indexes)
-            submit_btn = st.form_submit_button(label = 'Submit')
-        
-        matched_outer_folds = []
-        matched_folds = []
-        for outer_fold in outer_folds:
-            fold_impf = get_impf(outer_fold, alg, group)
-            if check_contain_features(df, fold_impf):
-                matched_outer_folds.append(outer_fold)
+            selected_fold = st.selectbox('Select a fold model: ', web_config.inner_folds)
+            submitted = st.form_submit_button(label = 'Submit')
+            if submitted:
+                form_0_submitted = True
+        if form_0_submitted:
+            st.write(f'*{sample_index} - {alg} - {group} - {selected_fold}*')
+            df = pd.DataFrame(df.loc[df.index == sample_index,:])
+            st.write('**Sample:**')
+            st.write(df)
 
-        for matched_outer_fold in matched_outer_folds:
-            outer_fold_index = matched_outer_fold.split('.')[0]
-            for i in range(1,6):
-                matched_fold = f'{outer_fold_index}.{i}'
-                matched_folds.append(matched_fold)
-        with st.form(key = 'form_1'):
-            selected_fold = st.selectbox('Select a fold model: ', matched_folds)
-            submit_btn = st.form_submit_button(label = 'Submit')
+            outer_fold = f"{selected_fold.split('.')[0]}.0"
+            df, n_impf = get_impf_dataframe(df, outer_fold, alg, group)
+            st.write('**Sample after CpG filtering:**')
+            st.write(df)
+            sample = np.expand_dims(np.array(df.iloc[0,:]).astype(float), axis = 0)
+            sample = torch.Tensor(sample).to(device)
 
-        selected_outer_fold = f"{selected_fold.split('.')[0]}.0"
-        df, n_impf = get_impf_dataframe(df, selected_outer_fold, alg, group)
-        st.write('**Sample after CpG filtering:**')
-        st.write(df)
-        sample = np.expand_dims(np.array(df.iloc[0,:]).astype(float), axis = 0)
-        sample = torch.Tensor(sample).to(device)
+            model = Impf_DNAMLP(n_impf, impf_cfg['n_classes'])
+            BEST_STATE_PATH = os.path.join(impf_cfg['MLP_BEST_STATES_DIR'], alg, group, f'{selected_fold}.pth')
+            
+            if torch.cuda.is_available() is False:
+                model.load_state_dict(torch.load(BEST_STATE_PATH, map_location = torch.device('cpu')))
+            else:
+                model.load_state_dict(torch.load(BEST_STATE_PATH))
+            model.to(device)
 
-        model = Impf_DNAMLP(n_impf, impf_cfg['n_classes'])
-        BEST_STATE_PATH = os.path.join(impf_cfg['MLP_BEST_STATES_DIR'], alg, group, f'{selected_fold}.pth')
-        
-        if torch.cuda.is_available() is False:
-            model.load_state_dict(torch.load(BEST_STATE_PATH, map_location = torch.device('cpu')))
-        else:
-            model.load_state_dict(torch.load(BEST_STATE_PATH))
-        model.to(device)
+            # Calculate diagnosis results and display
+            st.write('---  \n---')
+            res = softmax(model(sample), dim = 1)
+            columns = [group, 'Control']
+            row = res.detach().cpu().numpy()
+            res_df = pd.DataFrame(row)
+            res_df.columns = columns
+            st.write('### Diagnostic results')
+            col1, col2 = st.columns(2)
+            col1.write(f'|| **{group}**:  \n~{round(row[0][0]*100, 2)}%')
+            col2.write(f'|| **Non-tumour**:  \n~{round(row[0][1]*100, 2)}%')
 
-        # Calculate diagnosis results and display
-        st.write('---')
-        res = softmax(model(sample), dim = 1)
-        columns = [group, 'Control']
-        row = res.detach().cpu().numpy()
-        res_df = pd.DataFrame(row)
-        res_df.columns = columns
-        st.write('### Diagnostic results')
-        col1, col2 = st.columns(2)
-        col1.write(f'|| **{group}**:  \n~{round(row[0][0]*100, 2)}%')
-        col2.write(f'|| **Non-tumour**:  \n~{round(row[0][1]*100, 2)}%')
+            # Get ranked important features
+            st.write('---  \n---')
+            st.write('### CpG information')
+            ranked_impf = get_ranked_impf(df, outer_fold, alg, group)
+            ranked_impf = dict(sorted(ranked_impf.items(), key = lambda item:item[1]))
+            ranked_impf = list(ranked_impf.keys())
+            ranked_impf_df = pd.DataFrame()
+            ranked_impf_df['CpG'] = ranked_impf
+            ranked_impf_df['Priority'] = range(1, len(ranked_impf)+1)
+            st.write(f'**Important CpGs - {len(ranked_impf)} CpGs (the smaller the priority the more important)**')
 
-        # Get ranked important features
-        st.write('---')
-        st.write('### CpG information')
-        ranked_impf = get_ranked_impf(df, outer_fold, alg, group)
-        ranked_impf = dict(sorted(ranked_impf.items(), key = lambda item:item[1]))
-        ranked_impf = list(ranked_impf.keys())
-        ranked_impf_df = pd.DataFrame()
-        ranked_impf_df['CpG'] = ranked_impf
-        ranked_impf_df['Priority'] = range(1, len(ranked_impf)+1)
-        st.write(f'**Important CpGs - {len(ranked_impf)} CpGs (the smaller the priority the more important)**')
-
-        gb = GridOptionsBuilder.from_dataframe(ranked_impf_df)
-        gb.configure_selection('single', use_checkbox=True) 
-        gridOptions = gb.build()
-        grid_response = AgGrid(
-            ranked_impf_df,
-            gridOptions=gridOptions,
-            data_return_mode='AS_INPUT', 
-            update_mode='SELECTION_CHANGED', 
-            fit_columns_on_grid_load=True,
-            theme='material', #Add theme color to the table
-            height=350, 
-        )
-        try:
-            selected_cpg = grid_response['selected_rows'][0]['CpG']
-            st.write(selected_cpg)
-            # Get cpg information
-            cpg_info_path = web_config.cpg_info_path
-            cpg_df = get_cpg_info_file(cpg_info_path)
-            cpg_df = cpg_df
-            cpg_info = cpg_df.loc[cpg_df.Name == selected_cpg]
-            cpg_info = cpg_info.to_dict()
-            st.write(cpg_info)
-        except Exception as e:
-            st.write('*\*Select a CpG to know more information*')
-
-        # Display cpg information
-        st.write('---')
-        st.write('*Hung Nguyen, 2022*')
+            gb = GridOptionsBuilder.from_dataframe(ranked_impf_df)
+            gb.configure_selection('single', use_checkbox=True) 
+            gridOptions = gb.build()
+            grid_response = AgGrid(
+                ranked_impf_df,
+                gridOptions=gridOptions,
+                data_return_mode='AS_INPUT', 
+                update_mode='SELECTION_CHANGED', 
+                fit_columns_on_grid_load=True,
+                theme='material', #Add theme color to the table
+                height=350, 
+            )
+            try:
+                selected_cpg = grid_response['selected_rows'][0]['CpG']
+                st.write(selected_cpg)
+                # Get cpg information
+                cpg_info_path = web_config.cpg_info_path
+                cpg_df = get_cpg_info_file(cpg_info_path)
+                cpg_df = cpg_df
+                cpg_info = cpg_df.loc[cpg_df.Name == selected_cpg]
+                cpg_info = cpg_info.to_dict()
+                st.write(cpg_info)
+            except Exception as e:
+                st.write('*\*Select a CpG to know more information*')
     except Exception as e:
         st.write("Caught an error:", e)
 except:
--- Web/web_config.py
+++ Web/web_config.py
@@ -1,10 +1,22 @@
 import sys
 import os
 
+folds = ['1.0', '1.1', '1.2', '1.3', '1.4', '1.5', 
+        '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', 
+        '3.0', '3.1', '3.2', '3.3', '3.4', '3.5',
+        '4.0', '4.1', '4.2', '4.3', '4.4', '4.5',
+        '5.0', '5.1', '5.2', '5.3', '5.4', '5.5',]
+
 outer_folds = ['1.0', '2.0', '3.0', '4.0', '5.0']
+
+inner_folds = [fold for fold in folds if fold not in outer_folds]
+
 inner_fold_indexes = ['.1', '.2', '.3', '.4', '.5']
+
 algs = ['MLP', 'RF', 'LR', 'XGB']
+
 tissue_groups = [
     'Embryonal', 'Glioblastoma', 'Glio-neuronal', 'Sella', 'Ependymal', 'Other glioma', 'Nerve', 'Pineal', 'Mesenchymal', 'Melanocytic', 'Plexus', 'Glioma IDH', 'Haematopoietic', 'Unknown'
 ]
+
 cpg_info_path = '../Supplementary/humanmethylation450.csv'
